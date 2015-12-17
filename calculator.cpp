17c1e3b95b2f70d3a53aa2c4d9f078c10a7be237
Griffin <pokemonmaster248@gmail.com>
2015-12-16 20:41:21 -0500

    Initial Project

---
+++ calculator.cpp
@@ -0,0 +1,12 @@
+/*Griffin, 12/16/15, Calculating App*/
+/* Requirements, asks user for option 1-4. add, subtrack, mulitply, divide.
+Then ask the user for 2 numbers, and do teh appropraite calculation. */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  return 0;
+}
